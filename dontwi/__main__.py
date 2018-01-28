ce46fd121124add95922eb71732f8f3f0fd14a22
A.Shiomaneki <a.shiomaneki@gmail.com>
2018-01-28 17:54:41 +0900

    Add package configuration files

---
+++ dontwi/__main__.py
@@ -0,0 +1,7 @@
+#!  /usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from dontwi.dontwi import main
+
+
+main()
---
+++ setup.py
@@ -0,0 +1,26 @@
+#!  /usr/bin/python3
+# -*- coding: utf-8 -*-
+''' dontwi is a status transporter script from Mastodon instances to Twitter.
+
+See:
+https://github.com/vocalodon/dontwi
+'''
+from distutils.core import setup
+from os import path
+from dontwi.version import __version__
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.rst'), encoding='utf-8') as file:
+    long_description = file.read()
+
+setup(
+    name='dontwi',
+    version=__version__,
+    description='A status transporter from Mastodon instances to Twitter',
+    author='A.Shiomaneki',
+    author_email='a.shiomaneki@gmail.com',
+    url='https://github.com/vocalodon/dontwi',
+    long_description=long_description,
+    license='GNU General Public License v3.0',
+    packages=['dontwi']
+)
