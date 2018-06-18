4c52e9b869f29947320b6bd5e437d0298c4453a7
A.Shiomaneki <a.shiomaneki@gmail.com>
2018-06-19 00:23:08 +0900

    Update adding test for rate limit and multi status situation

--- dontwi/tests/test_dontwi.py
+++ dontwi/tests/test_dontwi.py
@@ -21,8 +21,7 @@ class TestDontwi(unittest.TestCase):
         self.your_mastodon_fqdn = YOUR_MASTODON_FQDN
         self.config, self.h_tag, self.tt_status, self.tw_status,\
             self.status_pr, self.result_log, self.status_str,\
-            self.summary = make_dummy_conf_and_result_log(
-                your_mastodon_fqdn=self.your_mastodon_fqdn)
+            self.summary = make_dummy_conf_and_result_log(your_mastodon_fqdn=self.your_mastodon_fqdn)
 
     def tearDown(self):
         remove_dummy_files()
@@ -36,27 +35,94 @@ class TestDontwi(unittest.TestCase):
 
     @patch.object(Twython,'update_status')
     def test_process_one_waiting_status(self, mocked_method):
-        mocked_method.return_value = '{"errors":[{"message":"Rate limit exceeded","code":429}]}'
+        mocked_method.return_value = dummy_tweet()
+
+        dontwi = Dontwi(self.config)
+        out_cn = TwitterConnector(self.config.outbound)
+        result_summaries = self.result_log.get_result_summaries_by_results(["Waiting"])
+        target = result_summaries[0]
+        target_inbound_status_id = target["inbound_status_id"]
+        dontwi.process_one_waiting_status(result_log=self.result_log, 
+                                            result_summary=target, media_ios=[], 
+                                            out_cn=out_cn,is_dry_run=False)
+        processed_summaries = self.result_log.get_result_summaries_by_results(["Succeed"])
+        target_summaries = [summary for summary in processed_summaries
+                            if summary["inbound_status_id"] == target_inbound_status_id]
+        self.assertEqual(len(target_summaries), 1)
+        self.assertEqual(target_summaries[0]["result"], "Succeed")
+
+    @patch.object(Twython,'update_status')
+    def test_process_one_waiting_status_at_rate_limit(self, mocked_method):
+        #mocked_method.return_value = '{"errors":[{"message":"Rate limit
+        #exceeded","code":429}]}'
         mocked_method.side_effect = TwythonRateLimitError("test", error_code=429, retry_after=5)
 
         dontwi = Dontwi(self.config)
         out_cn = TwitterConnector(self.config.outbound)
-        result_summaries = self.result_log.get_result_summaries_by_results(["Succeed"])
+        result_summaries = self.result_log.get_result_summaries_by_results(["Waiting"])
         target = result_summaries[0]
+        target_inbound_status_id = target["inbound_status_id"]
         dontwi.process_one_waiting_status(result_log=self.result_log, 
                                             result_summary=target, media_ios=[], 
                                             out_cn=out_cn,is_dry_run=False)
         processed_summaries = self.result_log.get_result_summaries_by_results(["Waiting"])
-        result_strs = [result["result"] for result in processed_summaries]
-        self.assertNotEquals(result_strs, "Failed")
-        target["status_string"] = [target["status_string"], target["status_string"]]
+        target_summaries = [summary for summary in processed_summaries
+                            if summary["inbound_status_id"] == target_inbound_status_id]
+        self.assertEqual(len(target_summaries), 1)
+        self.assertEqual(target_summaries[0]["result"], "Waiting")
+
+    @patch.object(Twython,'update_status')
+    def test_process_one_waiting_status_with_multi_status(self, mocked_method):
+        mocked_method.return_value = dummy_tweet()
+
+        dontwi = Dontwi(self.config)
+        out_cn = TwitterConnector(self.config.outbound)
+        result_summaries = self.result_log.get_result_summaries_by_results(["Waiting"])
+        target = result_summaries[0]
+        target_inbound_status_id = target["inbound_status_id"]
+        target["status_string"] = [target["status_string"]] * 5
+        status_strings_len = len(target["status_string"])
         dontwi.process_one_waiting_status(result_log=self.result_log, 
                                           result_summary=target, media_ios=[], 
                                           out_cn=out_cn,is_dry_run=False)
         result_strs = [result["result"] for result in result_summaries]
-        processed_summaries = self.result_log.get_result_summaries_by_results(["Waiting"])
-        self.assertEqual(len(processed_summaries), 1)
+        processed_summaries = self.result_log.get_result_summaries_by_results(["Succeed"])
+        target_summaries = [summary for summary in processed_summaries
+                            if summary["inbound_status_id"] == target_inbound_status_id]
+        self.assertEqual(len(target_summaries), status_strings_len)
+        assumed_results = ["Succeed"] * status_strings_len
+        target_summaries_results = [summary["result"] for summary in target_summaries]
+        self.assertEqual(target_summaries_results, assumed_results)
 
+    @patch.object(Twython,'update_status')
+    def test_process_one_waiting_status_with_multi_status_at_rate_limit(self, mocked_method):
+        multi_status_len = 10
+        succeed_len = 5
+        mocked_method.side_effect = [dummy_tweet()] * succeed_len + [TwythonRateLimitError("test", error_code=429, retry_after=5)]
+
+        dontwi = Dontwi(self.config)
+        out_cn = TwitterConnector(self.config.outbound)
+        result_summaries = self.result_log.get_result_summaries_by_results(["Waiting"])
+        target = result_summaries[0]
+        target_inbound_status_id = target["inbound_status_id"]
+        target["status_string"] = [target["status_string"]] * multi_status_len
+        status_strings_len = len(target["status_string"])
+        dontwi.process_one_waiting_status(result_log=self.result_log, 
+                                          result_summary=target, media_ios=[], 
+                                          out_cn=out_cn,is_dry_run=False)
+        result_strs = [result["result"] for result in result_summaries]
+        succeed_summaries = self.result_log.get_result_summaries_by_results(["Succeed"])
+        succeed_target_summaries = [summary for summary in succeed_summaries
+                            if summary["inbound_status_id"] == target_inbound_status_id]
+        self.assertEqual(len(succeed_target_summaries), succeed_len)
+        assumed_results = ["Succeed"] * succeed_len
+        target_summaries_results = [summary["result"] for summary in succeed_target_summaries]
+        self.assertEqual(target_summaries_results, assumed_results)
+        waiting_summaries = self.result_log.get_result_summaries_by_results(["Waiting"])
+        waiting_target_summaries = [summary for summary in waiting_summaries
+                            if summary["inbound_status_id"] == target_inbound_status_id]
+        self.assertEqual(len(waiting_target_summaries), 1)
+        self.assertEqual(len(waiting_summaries[0]["status_string"]), multi_status_len - succeed_len)
 
     @unittest.skipUnless(YOUR_MASTODON_FQDN,
                          'YOUR_MASTODON_FQDN isn\'t defined')
@@ -71,10 +137,10 @@ class TestDontwi(unittest.TestCase):
         self.assertFalse(is_ng)
         summaries_dc2 = {a_result: self.result_log.get_result_summaries_by_results([a_result])
                          for a_result in result_strs}
-        self.assertEqual(
-            len(summaries_dc2["Test"]) - len(summaries_dc["Test"]), 1)
-        self.assertGreater(len(summaries_dc2["Waiting"]),
-                           len(summaries_dc["Waiting"]))
+        self.assertEqual(len(summaries_dc2["Test"]) ,
+                         len(summaries_dc["Test"]) + 1)
+        self.assertLess(len(summaries_dc2["Waiting"]),
+                        len(summaries_dc["Waiting"]) + 1)
         self.assertEqual(len(summaries_dc2["Succeed"]),
                          len(summaries_dc["Succeed"]))
 
@@ -86,8 +152,7 @@ class TestDontwi(unittest.TestCase):
         should_have_secrets = Config()
         should_have_secrets.filename = self.config.filename
         should_have_secrets.load()
-        self.assertIsInstance(
-            self.config.items["endpoint your_mastodon"]["client_secret"], str)
+        self.assertIsInstance(self.config.items["endpoint your_mastodon"]["client_secret"], str)
         self.assertEqual(self.config.items["endpoint your_mastodon"]["client_secret"],
                          should_have_secrets.items["endpoint your_mastodon"]["client_secret"])
 
@@ -96,13 +161,11 @@ class TestDontwi(unittest.TestCase):
         dontwi = Dontwi(self.config)
         in_cn = dontwi.get_connector("inbound")
         out_cn = dontwi.get_connector("outbound")
-        if self.config.items["endpoint " + self.config.items["operation"]
-                             ["inbound"]]["type"] == "mastodon":
+        if self.config.items["endpoint " + self.config.items["operation"]["inbound"]]["type"] == "mastodon":
             self.assertIsInstance(in_cn, MastodonConnector)
         else:
             self.assertIsInstance(in_cn, TwitterConnector)
-        if self.config.items["endpoint " + self.config.items["operation"]
-                             ["outbound"]]["type"] == "mastodon":
+        if self.config.items["endpoint " + self.config.items["operation"]["outbound"]]["type"] == "mastodon":
             self.assertIsInstance(out_cn, MastodonConnector)
         else:
             self.assertIsInstance(out_cn, TwitterConnector)
